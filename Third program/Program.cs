8479fd2eb608a5eda39e394fc9d24e23ef8e18d3
ankit <ankitcool4370@gmail.com>
2022-10-31 20:48:34 +0530

    the hero

---
+++ Third program/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Third_program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ankit");
+            Console.WriteLine("i love a car");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
